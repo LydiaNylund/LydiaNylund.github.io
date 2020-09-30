8cbc5235064f3bb3c87a8714075f061109a681e8
LydiaNylund <71699338+LydiaNylund@users.noreply.github.com>
2020-09-30 14:41:58 +0300

    pokemon info js done

--- pokemon-info/script.js
+++ pokemon-info/script.js
@@ -1,7 +1,10 @@
 const pokemonInput = document.getElementById("pokemonInput");
 const searchButton = document.getElementById("button");
+let doYou;
+const info = document.getElementById("infoContainer");
+const still = document.getElementById("still");
 
-
+let name = document.getElementById("name");
 let pokeHeight = document.getElementById("height");
 let pokeWeight = document.getElementById("weight");
 let pokeType = document.getElementById("type");
@@ -11,19 +14,48 @@ let backPic = document.getElementById("backPic");
 searchButton.addEventListener("click", onSearchButtonClicked);
 
 function onSearchButtonClicked() {
-    let pokeName = pokemonInput.value
+    if(pokemonInput.value == "BRVR") {
+        player();
+    }
+    else {
+        let pokeName = pokemonInput.value;
 
-    fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
-    .then(response => response.json())
-    .then(data => getInfo(data))
+
+        fetch(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
+        .then(response => response.json())
+        .then(data => getInfo(data))
+    }
+
 }
 
 function getInfo(data) {
-    console.log(data)
+    if(doYou) {
+        info.classList.remove("over");
+        still.removeChild(doYou);
+    }
 
-    pokeHeight.innerHTML = data.height;
-    pokeWeight.innerHTML = data.weight;
-    pokeType.innerHTML = data.types[0].type.name;
-    frontPic = data.sprites.front_default;
-    backPic = data.sprites.back_default;
+    name.innerHTML = `Name: ${data.name}`
+    pokeHeight.innerHTML = `Height: ${data.height}`;
+    pokeWeight.innerHTML = `Weight: ${data.weight}`;
+    pokeType.innerHTML = `Type: ${data.types[0].type.name}`;
+    frontPic.innerHTML = `<img src="${data.sprites.front_default}"></img>`;
+    backPic.innerHTML = `<img src="${data.sprites.back_default}"></img>`;
+
+}
+function player() {
+
+    info.classList.add("over");
+    console.log(info)
+
+    name.innerHTML = "Pikachu";
+    pokeHeight.innerHTML = `Height: 4`;
+    pokeWeight.innerHTML = `Weight: 60`;
+    pokeType.innerHTML = `Type: electric`;
+    frontPic.innerHTML = `<img src="25-1.png"></img>`;
+    backPic.innerHTML = `<img src="25.png"></img>`;
+
+    doYou = document.createElement("div");
+    doYou.textContent = "Do you still love me?";
+    doYou.classList.add("big");
+    still.appendChild(doYou);
 }
