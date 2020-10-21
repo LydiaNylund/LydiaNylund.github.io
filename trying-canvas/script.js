9c07c17f2d05dfed343c6cc4c7c87fff6b4c4db9
LydiaNylund <71699338+LydiaNylund@users.noreply.github.com>
2020-10-21 13:22:42 +0300

    circle game finished

--- trying-canvas/script.js
+++ trying-canvas/script.js
@@ -1,11 +1,13 @@
 const canvas = document.getElementById("canvas");
+const counter = document.getElementById("counter");
+const gameOverElement = document.getElementById("gameOver");
+const gameOverMessage = document.getElementById("gameOverMessage");
 let key;
-let x = 200;
-let y = 200;
 let left;
 let right;
 let up;
 let down;
+let timerId;
 
 canvas.width = 800;
 canvas.height = 600;
@@ -15,32 +17,85 @@ let c = canvas.getContext("2d");
 window.addEventListener("keyup", keyUp);
 window.addEventListener("keydown", keyDown);
 
+let player = new Circle(200, 200, 40, "crimson");
+let coin = new Circle(400, 300, 10, "orange");
+let enemy = new Circle(300, 400, 30, "black");
+let points = 0;
+let isGameOver = false;
 
-window.requestAnimationFrame(renderCircle) 
+
+gameOverMessage.classList.add("hide")
+
+let lastFrameTime = new Date();
+
+window.addEventListener("load", () => {
+    window.requestAnimationFrame(renderCircle);
+})
+
 
 function renderCircle() {
+
+    let frameTime = new Date();
+    let deltaTime = (frameTime.getTime() - lastFrameTime.getTime()) / 1000;
+    lastFrameTime = frameTime;
+        
     c.clearRect(0, 0, canvas.width, canvas.height);
-    c.beginPath();
-    c.arc(x, y, 40, 0, Math.PI * 2, false);
-    c.fillStyle = "crimson";
-    c.fill();
+
+    counter.innerHTML = points;
+
+    player.draw();
+
 
     if(left === true) {
-        x--;
+        player.x -= 100 * deltaTime;
     }
     if(right === true) {
-        x++;
+        player.x += 100 * deltaTime;
     }
     if(up === true) {
-        y--;
+        player.y -= 100 * deltaTime;
     }
     if(down === true) {
-        y++;
+        player.y += 100 * deltaTime;
     }
  
+    
+    coin.draw();
 
-    window.requestAnimationFrame(renderCircle) 
+    
+    if(player.overlaps(coin)) {
+        coin.x = Math.floor(Math.random() * 800)
+        coin.y = Math.floor(Math.random() * 600)
+        points++;
+    }
+
+
+    enemy.draw();
+
+    if(player.x < enemy.x) {
+        enemy.x -= 50 * deltaTime;
+    }
+    else {
+        enemy.x += 50 * deltaTime;
+    }
+    if(player.y < enemy.y) {
+        enemy.y -= 50 * deltaTime;
+    }
+    else {
+        enemy.y += 50 * deltaTime;
+    }
+
+    if(player.overlaps(enemy)) {
+        onGameOver();
+    }
+    else if(isGameOver === false) {
+        window.requestAnimationFrame(renderCircle) 
+    }
+    
+
 }
+
+
 
 
 function keyDown(event) {
@@ -78,4 +133,49 @@ function keyUp(event) {
     }
 }
 
-renderCircle()
+function pressSpace(event) {
+    if(event.key === " ") {
+        canvas.style.backgroundColor = "white";
+        player.color = "crimson";
+        enemy.color = "black";
+        coin.color = "orange";
+        counter.style.color = "black";
+        gameOverElement.classList.add("hide");
+        gameOverMessage.classList.add("hide");
+
+        player.x = 200;
+        player.y = 200;     
+        enemy.x = 300;
+        enemy.y = 400;
+        coin.x = 400;
+        coin.y = 300;
+
+        points = 0;
+        isGameOver = false;
+
+        clearTimeout(timerId);
+        window.removeEventListener("keydown", pressSpace);
+
+        window.requestAnimationFrame(renderCircle);
+    }
+
+}
+
+
+function onGameOver() {
+    canvas.style.backgroundColor = "rgb(44, 44, 44)";
+    player.color = "rgb(95, 51, 54)";
+    enemy.color = "rgb(29, 29, 29)";
+    coin.color = "rgb(87, 67, 49)";
+    counter.style.color = "gold";
+    gameOverElement.classList.remove("hide");
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    
+    timerId = setTimeout(`gameOverMessage.classList.remove("hide")`, 1000);
+    player.draw();
+    coin.draw();
+    enemy.draw();
+
+    window.addEventListener("keydown", pressSpace);
+}
+
