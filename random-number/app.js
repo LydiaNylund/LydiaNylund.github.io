e160b38d8c7ab08a7db4fa2ebbf9d4c41b82f6e7
LydiaNylund <71699338+LydiaNylund@users.noreply.github.com>
2021-01-14 12:40:18 +0200

    random-number_last_uppdate

--- random-number/app.js
+++ random-number/app.js
@@ -1,12 +1,19 @@
-let numberOfNumbers = 5;
+let numberOfNumbers = 6;
+let numberArray = [];
+
+let timesClickedSpin = 0;
+let sameNumber = 0;
 
 const numbersOutput = document.getElementById("numbers");
 const locksOutput = document.getElementById("locks");
 const spinButton = document.getElementById("spin");
+const winOutput = document.getElementById("winOutput");
+const resetButton = document.getElementById("reset");
 
 spinButton.addEventListener("click", spinButtonClicked);
+resetButton.addEventListener("click", resetButtonClicked);
 
-let numberArray = [];
+
 for(let i = 0; i < numberOfNumbers; i++) {    
     numberArray.push({
         num: 0,
@@ -22,12 +29,27 @@ for(let i = 0; i < numberOfNumbers; i++) {
     numbersOutput.appendChild(numberArray[i].display);
 }
 
+
 function spinButtonClicked() {
+    timesClickedSpin++;
+    sameNumber = 0;
+
     for(let i = 0; i < numberOfNumbers; i++) {
         if(!numberArray[i].locked) {
             numberArray[i].num = (Math.floor(Math.random() * 10));
             numberArray[i].display.innerHTML = numberArray[i].num;
         }
+        if(numberArray[0].num === numberArray[i].num) {
+            sameNumber++;
+        }
+    }
+    winOutput.innerHTML = "";
+    checkWin();
+}
+
+function checkWin() {
+    if(sameNumber === numberOfNumbers) {
+        winOutput.innerHTML = `Du har vunnit! ${"<br>"} Du klickade spin ${timesClickedSpin} gånger.`;
     }
 }
 
@@ -42,5 +64,14 @@ function lockButtonClicked(event) {
                 numberArray[i].display.style.color = "grey";
             }
         }
+    }
+}
+
+function resetButtonClicked() {
+    timesClickedSpin = 0;
+    winOutput.innerHTML = "";
+    for(let i = 0; i < numberOfNumbers; i++) {
+        numberArray[i].num = 0;
+        numberArray[i].display.innerHTML = numberArray[i].num;
     }
 }
